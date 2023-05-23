110ae7ba3b21ef972c7d58216d983e03824bb2b9
Erickkirui <erickkirui653@gmail.com>
2023-05-23 16:20:56 +0300

    complete anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        result = []
+        for word in word_list:
+            if self.is_anagram(word):
+                result.append(word)
+        return result
+
+    def is_anagram(self, word):
+        return sorted(self.word.lower()) == sorted(word.lower())
